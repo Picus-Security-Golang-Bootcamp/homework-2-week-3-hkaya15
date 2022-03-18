367d943e170e13e0fe2199a7d5d87665ff22bd6b
hkaya15 <kayahuseyin15@gmail.com>
2022-03-18 17:07:20 +0300

    Change return types

--- product/product.go
+++ product/product.go
@@ -1,11 +1,13 @@
 package product
 
+import "github.com/hkaya15/PicusSecurity/Week_3_Homework/book"
+
 type IProduct interface {
-	Create() IProduct
-	GetList() []IProduct
+	Create() book.Book
+	GetList() []book.Book
 	AddList()
-	SearchById(id int) IProduct
-	SearchByName(name string) []IProduct
+	SearchById(id int) book.Book
+	SearchByName(name string) []book.Book
 	DeleteById(id int)
 	BuyById(id int, count int) 
 }
